ef199a520ad504e657ca84712f245f1e77f982b8
Diogo <diogomqbm13@gmail.com>
2018-04-23 15:06:11 -0300

    Making Book.js and BookShelf.js stateless functions

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import BookShelfChanger from './BookShelfChanger'
 import '../App.css'
 import noCoverImage from '../icons/no-cover-image.png'
 
-export default class Book extends Component {
-    render(){
-
-        const coverImg = this.props.book.imageLinks ? this.props.book.thumbnail : noCoverImage
-        const title = this.props.book.title ? this.props.book.title : "No title available"
+export default function Book(props) {
+        const coverImg = props.book.imageLinks ? props.book.thumbnail : noCoverImage
+        const title = props.book.title ? props.book.title : "No title available"
 
         return (
             <li>
@@ -16,19 +14,18 @@ export default class Book extends Component {
                 <div className="book-cover" 
                 style={{ width: 128, height: 193, backgroundImage: `url(${coverImg})` }}></div>
                  <BookShelfChanger
-                    book={ this.props.book }
-                    books={ this.props.books }
-                    changeShelf={ this.props.changeShelf }
+                    book={ props.book }
+                    books={ props.books }
+                    changeShelf={ props.changeShelf }
                  />
               </div>
               <div className="book-title">{ title }</div>
               {
-                  this.props.book.authors && this.props.book.authors.map((author, index) => (
+                  props.book.authors && props.book.authors.map((author, index) => (
                         <div className="book-authors" key={index}>{author}</div>
                   ))
               }
             </div>
           </li>
         )
-    }
-}
+      }
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../App.css'
 import Book from './Book'
 
-export default class Bookshelf extends Component {
-    render(){
+export default function Bookshelf(props) {
         return (
             <ol className="books-grid">
-                {this.props.books.map(book => (
+                {props.books.map(book => (
                     <Book
                         book={ book }
-                        books={ this.props.books }
+                        books={ props.books }
                         key={ book.id }
-                        changeShelf={ this.props.changeShelf }
+                        changeShelf={ props.changeShelf }
                     />
                 ))}
             </ol>
         )
     }
-}
